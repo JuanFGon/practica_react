6ea048fa02e367b7cc1711d0c0d01c199322b7c1
Juan Felipe Gonzalez <juan.fel.gonzalez+01@hotmail.com>
2026-08-24 11:40:47 -0500

    Productos

---
+++ src/data/productos.js
@@ -0,0 +1,6 @@
+export const productos = [
+    { nombre: "Mouse", precio: 20, stock: 5, imagen: "/Imagenes/mouse.jpeg" },
+    { nombre: "Teclado", precio: 45, stock: 0, imagen: "/Imagenes/teclado.jpeg" },
+    { nombre: "Monitor", precio: 150, stock: 3, imagen: "/Imagenes/monitor.jpeg" },
+    { nombre: "Webcam", precio: 30, stock: 8, imagen: "/Imagenes/web.jpeg" },
+];
